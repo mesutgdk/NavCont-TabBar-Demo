c4e554dd13701fb7b71560118385e881cb0fcdc2
mesutgdk <mesuttgedik@gmail.com>
2023-05-27 14:58:58 +0300

    New project is created

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# NavCont-TabBar-Demo
+# NavigationcontrollerDemo
+
+## UINavigationController
+
+* Present-Dismiss
+Present a screen, old one is still behind the screen, u can see and pull below to exit
+
+* Push-Pop
+Push a new screen, old one is still there at background but not on screen,
+Just Pop and old one comes
+It is a stack
---
+++ TabBarDemo/TabBarDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TabBarDemo
+//
+//  Created by Mesut Gedik on 27.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
